d66ce0743692943dc1348e6a88b00340ab418de1
yemiwebby <yemiwebby@gmail.com>
2022-11-11 09:31:28 +0000

    Modified package name

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ DESCRIPTION = 'A conversion package'
 LONG_DESCRIPTION = 'A package that makes it easy to convert values between several units of measurement'
 
 setup(
-    name="convrsn",
+    name="convsn",
     version=VERSION,
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
